e77e874c2c71cfdae0dc1fded8e0310eb26a3179
SMJ1227 <joy001227@gmail.com>
2023-03-07 14:01:23 +0900

    SCR CH.1

---
+++ test.py
@@ -0,0 +1,31 @@
+print('test' .capitalize())
+print("test".upper())
+
+print(10+20)
+
+a=1
+print(type(a))
+print(a)
+
+print('hello world')
+
+if __name__ == '__main__':
+    print('hello world')
+
+if a==1:
+    print(1)
+    print(2)
+    print(3)
+else:
+    print(4)
+
+if a==1:
+    print(1); print(2)
+
+a = (1+
+        2+
+    3+
+4)
+print(a)
+
+
