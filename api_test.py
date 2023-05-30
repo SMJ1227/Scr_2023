4cacb1af6a61787eb4965d243bd3b9f52f55db4e
SMJ1227 <joy001227@gmail.com>
2023-05-30 16:24:20 +0900

    api연결

--- api_test.py
+++ api_test.py
@@ -6,6 +6,7 @@ params = {'serviceKey': 'ZFXmFX0IXF2lZtUXaZW+lLjQUBH3AFz14p6BfEC8AFWev/Xplh7PYsa
 response = requests.get(url, params=params)
 print(response.content)
 '''
+'''
 import os
 import sys
 import urllib.request
@@ -20,6 +21,27 @@ request.add_header("X-Naver-Client-Secret",client_secret)
 response = urllib.request.urlopen(request)
 rescode = response.getcode()
 if rescode == 200:
+    response_body = response.read()
+    print(response_body.decode('utf-8'))
+else:
+    print("Error Code:" + rescode)
+'''
+# 네이버 검색 API 예제 - 블로그 검색
+import os
+import sys
+import urllib.request
+client_id = "haTmx_2L7UB1bo68x0d8"
+client_secret = "Zf0wkFCKHv"
+encText = urllib.parse.quote("미국 국기 jpg")
+#url = "https://openapi.naver.com/v1/search/blog?query=" + encText # JSON 결과
+url = "https://openapi.naver.com/v1/search/image.xml?query=" + encText + "&display=10&sort=sim"# xml
+
+request = urllib.request.Request(url)
+request.add_header("X-Naver-Client-Id", client_id)
+request.add_header("X-Naver-Client-Secret", client_secret)
+response = urllib.request.urlopen(request)
+rescode = response.getcode()
+if(rescode==200):
     response_body = response.read()
     print(response_body.decode('utf-8'))
 else:
